bd014b8f177b7472435baabf2b79d4906e50a1aa
SuhaniPatel88 <115473374+SuhaniPatel88@users.noreply.github.com>
2024-03-06 09:44:06 +0530

    Update FileXtractor.sh

--- FileXtractor.sh
+++ FileXtractor.sh
@@ -3,27 +3,43 @@ read link
 echo "Enter the filename where you want to save the data: "
 read file
 curl $link > ~/$file
-grep -o 'http[^"]*\.\(png\|jpg\|jpeg\|svg\)' ~/$file > imagefile.txt
-grep -o 'http[^"]*\.\(gif\|mp4\|mov\|avi\|flv\)' ~/$file > videofile.txt
-grep -o 'http[^"]*\.\(mp3\|ogg\|wav\)' ~/$file > audiofile.txt
-image_file='imagefile.txt'
-c=1
-while read line;
-do
-curl -o ~/Pictures/image$c $line
-(( c++ ))
-done < $image_file
 
-video_file='videofile.txt'
-c=1
-while read line; do
-  curl -o ~/Videos/video$c $line
-  ((c++))
-done < $video_file
+echo "Select the type of files to download:"
+echo "1. Images"
+echo "2. Videos"
+echo "3. Audio"
+read choice
 
-audio_file='audiofile.txt'
-c=1
-while read line; do
-  curl -o ~/Music/audio$c $line
-  ((c++))
-done < $audio_file
+case $choice in
+  1)
+    grep -o 'http[^"]*\.\(png\|jpg\|jpeg\|svg\)' ~/$file > imagefile.txt
+    image_file='imagefile.txt'
+    c=1
+    while read line;
+    do
+      curl -o ~/Pictures/image$c $line
+      (( c++ ))
+    done < $image_file
+    ;;
+  2)
+    grep -o 'http[^"]*\.\(gif\|mp4\|mov\|avi\|flv\)' ~/$file > videofile.txt
+    video_file='videofile.txt'
+    c=1
+    while read line; do
+      curl -o ~/Videos/video$c $line
+      ((c++))
+    done < $video_file
+    ;;
+  3)
+    grep -o 'http[^"]*\.\(mp3\|ogg\|wav\)' ~/$file > audiofile.txt
+    audio_file='audiofile.txt'
+    c=1
+    while read line; do
+      curl -o ~/Music/audio$c $line
+      ((c++))
+    done < $audio_file
+    ;;
+  *)
+    echo "Invalid choice. Please select a valid option."
+    ;;
+esac
